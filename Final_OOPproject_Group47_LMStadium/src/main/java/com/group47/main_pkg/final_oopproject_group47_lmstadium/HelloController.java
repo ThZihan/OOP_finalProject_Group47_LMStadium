9432ff9943a47f543c25888d7d6641f39c7e0478
Tahfizul Hasan Zihan <tahfijulhassanjehan@gmail.com>
2024-07-17 14:27:06 +0600

    firstCommit

---
+++ Final_OOPproject_Group47_LMStadium/src/main/java/com/group47/main_pkg/final_oopproject_group47_lmstadium/HelloController.java
@@ -0,0 +1,14 @@
+package com.group47.main_pkg.final_oopproject_group47_lmstadium;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ Final_OOPproject_Group47_LMStadium/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.group47.main_pkg.final_oopproject_group47_lmstadium {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.group47.main_pkg.final_oopproject_group47_lmstadium to javafx.fxml;
+    exports com.group47.main_pkg.final_oopproject_group47_lmstadium;
+}
